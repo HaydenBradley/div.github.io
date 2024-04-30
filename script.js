e131ae14bda6414adae2024a12989ceab1f89e09
Nicholas Bradley <bradleyhayden6932@gmail.com>
2024-04-29 21:41:33 -0500

    made button work to bring box back

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
-document.getElementById('controlButton').addEventListener('click', function() {
-    var box = document.getElementById('box');
-    var containerWidth = document.getElementById('container').clientWidth;
-    var boxWidth = box.offsetWidth;
-  
-    var newX = containerWidth - boxWidth - 20; 
-  
- 
-    box.style.transform = 'translateX(' + newX + 'px)';
-    
+var isBoxMoved = false;
 
-    if (box.style.transform === 'translateX(' + newX + 'px)') {
-      setTimeout(function() {
-        box.style.transform = 'translate(-50%, -50%)';
-      }, 500); 
-    }
-  });
-  
+document.getElementById('controlButton').addEventListener('click', function() {
+  var box = document.getElementById('box');
+  var containerWidth = document.getElementById('container').clientWidth;
+  var boxWidth = box.offsetWidth;
+
+  var newX = containerWidth - boxWidth - 20; 
+
+  if (!isBoxMoved) {
+    box.style.transform = 'translateX(' + newX + 'px)';
+    isBoxMoved = true;
+  } else {
+    box.style.transform = 'translate(-50%, -50%)';
+    isBoxMoved = false;
+  }
+});
